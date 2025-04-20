2ff5ebfc34e2d1c943f306923e9ed2b0c9d845b3
yatharthyadu <yatharth.yadu66@gmail.com>
2025-04-20 18:35:30 +0530

    module ui development

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,23 +1,27 @@
-import MealsGrid from '@/components/meals/meals-grid'
-import classes from "./page.module.css"
-import Link from 'next/link'
-import React from 'react'
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+import React from "react";
 
 const MealsPage = () => {
   return (
     <>
-    
-    <header className={classes.header}>
-      <h1>Delicious meals, created <span className={classes.highlight}>by you</span> </h1>
-    <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
-   <p className={classes.cta}>
-    <Link href="/meals/share"> Share your Favorite Recipes </Link>
-   </p>
-    </header>
-    <main className={classes.main}></main>
-    <MealsGrid meals={[]}/>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>{" "}
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share"> Share your Favorite Recipe </Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+      <MealsGrid meals={[]} />
     </>
-  )
-}
+  );
+};
 
-export default MealsPage
+export default MealsPage;
