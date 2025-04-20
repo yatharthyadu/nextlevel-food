e4319d4771b8b94a7bc88b111b6670cce13d2266
yatharthyadu <yatharth.yadu66@gmail.com>
2025-04-20 18:16:51 +0530

    Created UI

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,22 @@
+import MealsGrid from '@/components/meals/meals-grid'
+import classes from "./page.module.css"
+import Link from 'next/link'
 import React from 'react'
 
 const MealsPage = () => {
   return (
-    <div>MealsPage</div>
+    <>
+    
+    <header className={classes.header}>
+      <h1>Delicious meals, created <span className={classes.highlight}>by you</span> </h1>
+    <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+   <p className={classes.cta}>
+    <Link href="/meals/share"> Share your Favorite Recipe </Link>
+   </p>
+    </header>
+    <main className={classes.main}></main>
+    <MealsGrid meals={[]}/>
+    </>
   )
 }
 
--- components/main-header.js
+++ components/main-header.js
@@ -1,22 +1,29 @@
-import Link from "next/link"
-import logoImg from "@/assets/logo.png"
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "@/components/main-header-background";
 
-export default function Header(){
-
-    return <header>
-        <Link href={"/"}>
-        <img src={logoImg.src} alt="A plate with food on it"/>
-        Next Level food
+export default function Header() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href={"/"}>
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          Next Level food
         </Link>
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/meals">Browse Meals</Link>
-                </li>
-                <li>
-                    <Link href="/community">Foodies Community</Link>
-                </li>
-            </ul>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
         </nav>
-    </header>
+      </header>
+    </>
+  );
 }
