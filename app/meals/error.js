11428b1fd96a5207d8eda1886bf0e46a1ce8cb9d
yatharthyadu <yatharth.yadu66@gmail.com>
2025-04-24 04:33:53 +0530

    Linked Backend and Binded data for meals and meals single page

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+"use client"
+
+export default function Error(){
+    return  <main className="error">
+     <h1>An Error Occured !</h1>
+     <p>Failed to fetch Meal Data, Please try again later</p>
+    </main>
+}
